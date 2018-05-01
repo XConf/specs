2f52a5742feaf64c4b85a3e736e6a7fdc69840f7
zetavg <mail@zeta.vg>
2018-05-01 21:54:47 +0800

    add GraphiQL

--- function.js
+++ function.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const { graphqlExpress } = require('apollo-server-express');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const mockServer = require('./mock-server/index');
 
 const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: mockServer.schema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 exports.graphql = (request, response) => {
   request.url = '/graphql';
   request.path = '/graphql';
   request.method = 'POST';
+  app(request, response);
+};
+
+exports.graphiql = (request, response) => {
+  request.url = '/graphiql';
+  request.path = '/graphiql';
+  request.method = 'GET';
   app(request, response);
 };
